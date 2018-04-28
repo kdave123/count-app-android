482d573d45cdb5ad287cfc66762a91266b30bee4
kdave123 <38814533+kdave123@users.noreply.github.com>
2018-04-28 21:33:00 +0530

    Application .java Source code
    Counting Application Code

---
+++ BuildConfig.java
@@ -0,0 +1,11 @@
+package theway.count;
+
+public final class BuildConfig
+{
+  public static final String APPLICATION_ID = "theway.count";
+  public static final String BUILD_TYPE = "debug";
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
